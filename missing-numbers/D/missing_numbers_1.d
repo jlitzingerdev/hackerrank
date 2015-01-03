0e554ff8e3c5dae69ec53bde1d7b037e698df79a
Jason Litzinger <jlitzingerdev@gmail.com>
2015-01-03 12:54:31 -0700

    Add D lang solution.  Only tested against input provided and when lengths equal.

--- missing-numbers/D/missing_numbers_1.d
+++ missing-numbers/D/missing_numbers_1.d
@@ -3,21 +3,62 @@ import std.array;
 import std.conv;
 import std.algorithm;
 
+int[] missingNumbers(int[] A, int[] B)
+{
+    if (A.length == B.length) {
+        return [];
+    }
+
+    int min = 99999;
+    int max = 0;
+
+    foreach (int i; B) {
+        if (i > max) {
+            max = i;
+        }
+        if (i < min) {
+            min = i;
+        }
+    }
+
+    int[] t = new int[(max - min) + 1];
+    
+    foreach(int i; B) {
+        t[i % min]++;
+    }
+
+    foreach(int i; A) {
+        t[i % min]--;
+    }
+
+    int[] res;
+    foreach(int i,v ; t) {
+        if (v > 0) {
+            res ~= i + min;
+        }
+    }
+    
+    return res;
+}
+
+
 void main()
 {
     int sizeA;
     int sizeB;
 
     readf(" %d ", &sizeA);
-    writeln("sizeA=", sizeA);
     string[] vals = split(stdin.readln());
-    // The wrapping in an array is due to map's lazy eval
+    // The cast to an array is due to map's lazy eval
     int[] A = array(vals.map!(to!int));
     
     readf(" %d ", &sizeB);
-    writeln("sizeB=", sizeB);
     vals = split(stdin.readln());
     int[] B = array(vals.map!(to!int));
 
-
+    int[] res = missingNumbers(A,B);
+    foreach (int i; res) {
+        writef("%d ", i);
+    }
+    write("\n");
 }
